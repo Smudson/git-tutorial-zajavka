eba188241840ac4f1e724450d62deb1b8c714458
cubas <myMail@gmail.com>
2023-05-15 22:56:31 +0200

    feature-commit-1

--- Person.java
+++ Person.java
@@ -1,11 +1,16 @@
 import java.math.BigDecimal;
 
 public class Person {
+    public static final String HEIGHT = "100";
     private String name;
     private BigDecimal salary;
+    private String pesel;
 
     public Integer age(){
         return 40;
     }
-    public static final String HEIGHT = "100";
+
+    public String whatIsYourHobby(){
+        return "pizza";
+    }
 }
