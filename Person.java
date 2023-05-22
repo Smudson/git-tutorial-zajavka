c95f4edeaf5e9f136dec6e38b5f1bf1ada252a15
Jakub Smuda <cubasmuda@gmail.com>
2023-05-23 00:21:28 +0200

    change2

--- Person.java
+++ Person.java
@@ -7,12 +7,6 @@ public class Person {
         System.out.println("Hello!");
         System.out.println("Hello!");
         System.out.println("Hello!");
-        System.out.println("Hello!");
-        System.out.println("Hello!");
-        System.out.println("Hello!");
-        System.out.println("Hello!");
-        System.out.println("Hello!");
-        System.out.println("Hello!");
 
     }
 }
