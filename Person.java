ea45b43764775aff3585879b9725d89c755a446b
Jakub Smuda <cubasmuda@gmail.com>
2023-05-23 00:21:16 +0200

    change1

--- Person.java
+++ Person.java
@@ -1,26 +1,18 @@
 import java.math.BigDecimal;
 
 public class Person {
-    public static final String HEIGHT = "100";
-    private String name;
-    private BigDecimal salary;
-    private String pesel;
-    private String surname;
-
-    public Integer age(){
-        return 40;
-    }
-
-    public String whatIsYourHobby(){
-        return "pizza";
-    }
-
     public void hello(){
         System.out.println("Hello!");
-        System.out.println("Hello2!");
-        System.out.println("Hello3!");
-        System.out.println("Hello4!");
-        System.out.println("Hello5!");
-        System.out.println("Hello6!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+
     }
 }
